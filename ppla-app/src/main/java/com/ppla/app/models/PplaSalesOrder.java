8889d08a1375697bf97e0ce066f68a532fa4eb75
Mark <lordmarkm@yahoo.com>
2014-06-24 09:28:04 +0800

    ?

---
+++ ppla-app/src/main/java/com/ppla/app/models/PplaSalesOrder.java
@@ -0,0 +1,10 @@
+package com.ppla.app.models;
+
+import javax.persistence.Entity;
+
+import com.tyrael.process.mgt.models.order.SalesOrder;
+
+@Entity(name = "SALES_ORDER")
+public class PplaSalesOrder extends SalesOrder {
+
+}
