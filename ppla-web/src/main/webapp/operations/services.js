f3b666d4667713880fbd4d8ecae4f0b3adfb029a
mbmartinez <mbmartinez@exist.com>
2014-10-30 13:47:40 +0800

    services

--- ppla-web/src/main/webapp/operations/services.js
+++ ppla-web/src/main/webapp/operations/services.js
@@ -4,60 +4,60 @@ define(['angular'], function(angular) {
   angular.module('app.services', ['ngResource'])
 
   .factory('AuthService', function($resource) {
-    return $resource('/auth');
+    return $resource('../auth');
   })
   .factory('SalesOrderService', function($resource) {
-    return $resource('/salesorder/:trackingNo', {}, {
+    return $resource('../salesorder/:trackingNo', {}, {
       page: {method: 'GET', isArray: false}
     });
   })
   .factory('OrderItemService', function($resource) {
-    return $resource('/orderItem/:action/:id');
+    return $resource('../orderItem/:action/:id');
   })
   .factory('WorkOrderService', function($resource) {
-    return $resource('/workOrder/:action/:orderItemId/:trackingNo', {}, {
+    return $resource('../workOrder/:action/:orderItemId/:trackingNo', {}, {
       page: {method: 'GET', isArray: false},
       close: {method: 'POST', isArray: false, params: {action: 'close'}},
       findByTag: {method: 'GET', isArray: false, params: {action: 'tag'}}
     });
   })
   .factory('ProductService', function($resource) {
-    return $resource('/product/:id', {}, {
+    return $resource('../product/:id', {}, {
       page: {method: 'GET', isArray: false}
     });
   })
   .factory('MaterialService', function($resource) {
-    return $resource('/material/:type/:source');
+    return $resource('../material/:type/:source');
   })
   .factory('WoMaterialService', function($resource) {
-    return $resource('/womaterials/:trackingNos/:source');
+    return $resource('../womaterials/:trackingNos/:source');
   })
   .factory('ProcessService', function($resource) {
-    return $resource('/process');
+    return $resource('../process');
   })
   .factory('WarehouseProcessService', function($resource) {
-    return $resource('/warehouse');
+    return $resource('../warehouse');
   })
   .factory('MixingProcessService', function($resource) {
-    return $resource('/mixing/:action/:id');
+    return $resource('../mixing/:action/:id');
   })
   .factory('ExtrusionProcessService', function($resource) {
-    return $resource('/extrusion/:action/:id');
+    return $resource('../extrusion/:action/:id');
   })
   .factory('PrintingProcessService', function($resource) {
-    return $resource('/printing/:action/:id');
+    return $resource('../printing/:action/:id');
   })
   .factory('CuttingProcessService', function($resource) {
-    return $resource('/cutting/:action/:id');
+    return $resource('../cutting/:action/:id');
   })
   .factory('PplaUserService', function($resource) {
-    return $resource('/user');
+    return $resource('../user');
   })
   .factory('MachineService', function($resource) {
-    return $resource('/machine/:type');
+    return $resource('../machine/:type');
   })
   .factory('ProcessMaterialStackService', function($resource) {
-    return $resource('/stack/:tag')
+    return $resource('../stack/:tag')
   });
 
 });
