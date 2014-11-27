014d69ec2b9f753342ad59dae0dd3c575baabb52
Markm <lordmarkm@gmail.com>
2014-11-27 16:28:24 +0800

    Properties adjusted for production env

--- ppla-app/src/main/resources/META-INF/sql/accounts.sql
+++ ppla-app/src/main/resources/META-INF/sql/accounts.sql
@@ -1,7 +1,7 @@
-insert into accounts(authorities,password,username) values('ROLE_ADMIN','123qwe','admin');
-insert into accounts(authorities,password,username) values('ROLE_OPERATOR', '123qwe', 'op');
-insert into accounts(authorities,password,username) values('ROLE_WAREHOUSE', '123qwe', 'warehouse');
-insert into accounts(authorities,password,username) values('ROLE_MIXER', '123qwe', 'mixer');
-insert into accounts(authorities,password,username) values('ROLE_EXTRUDER', '123qwe', 'extruder');
-insert into accounts(authorities,password,username) values('ROLE_PRINTER', '123qwe', 'printer');
-insert into accounts(authorities,password,username) values('ROLE_CUTTER', '123qwe', 'cutter');
+insert into accounts(id,authorities,password,username) values(1,'ROLE_ADMIN','123qwe','admin');
+insert into accounts(id,authorities,password,username) values(2,'ROLE_OPERATOR', '123qwe', 'op');
+insert into accounts(id,authorities,password,username) values(3,'ROLE_WAREHOUSE', '123qwe', 'warehouse');
+insert into accounts(id,authorities,password,username) values(4,'ROLE_MIXER', '123qwe', 'mixer');
+insert into accounts(id,authorities,password,username) values(5,'ROLE_EXTRUDER', '123qwe', 'extruder');
+insert into accounts(id,authorities,password,username) values(6,'ROLE_PRINTER', '123qwe', 'printer');
+insert into accounts(id,authorities,password,username) values(7,'ROLE_CUTTER', '123qwe', 'cutter');
--- ppla-quickbooks/src/main/java/com/ppla/quickbooks/unmarshaller/SalesorderQueryResponseProcessorz.java
+++ ppla-quickbooks/src/main/java/com/ppla/quickbooks/unmarshaller/SalesorderQueryResponseProcessorz.java
@@ -19,9 +19,12 @@ import com.ppla.app.models.PplaSalesOrder;
 import com.ppla.app.services.PplaPersonService;
 import com.ppla.app.services.PplaProductService;
 import com.ppla.app.services.PplaSalesOrderService;
+import com.ppla.quickbooks.entity.InventoryItem;
 import com.ppla.quickbooks.entity.generated.SalesOrderLineRet;
 import com.ppla.quickbooks.entity.generated.SalesOrderQueryRsType;
 import com.ppla.quickbooks.entity.generated.SalesOrderRet;
+import com.ppla.quickbooks.reference.PplaInventoryType;
+import com.ppla.quickbooks.service.InventoryItemService;
 
 /**
  * @author markm
@@ -40,6 +43,9 @@ public class SalesorderQueryResponseProcessorz {
     @Autowired
     private PplaProductService productService;
 
+    @Autowired
+    private InventoryItemService inventoryService;
+
     public void processSalesOrderQueryResponse(SalesOrderQueryRsType salesOrderQueryRsType) {
         List<SalesOrderRet> salesOrders = salesOrderQueryRsType.getSalesOrderRet();
         for (SalesOrderRet salesOrder : salesOrders) {
@@ -57,8 +63,9 @@ public class SalesorderQueryResponseProcessorz {
         PplaSalesOrder so = new PplaSalesOrder();
         so.setDateCreated(DateTime.now());
         so.setEditSequence(salesOrderRet.getEditSequence());
-        so.setTrackingNo(RandomStringUtils.random(5));
+        so.setTrackingNo(RandomStringUtils.randomAlphanumeric(5));
         so.setTxnNumber(salesOrderRet.getTxnNumber());
+        so.setTimeModified(DateTime.now());
 
         //Shipping address
         if (null != salesOrderRet.getShipAddressBlock()) {
@@ -70,8 +77,10 @@ public class SalesorderQueryResponseProcessorz {
         if (null == customer) {
             Name name = new Name();
             name.setSurname(salesOrderRet.getCustomerRef().getFullName());
+            name.setGivenName("");
             customer = new PplaPerson();
             customer.setName(name);
+            customer.setListId(salesOrderRet.getCustomerRef().getListID());
             personService.save(customer);
         }
         so.setCustomer(customer);
@@ -82,8 +91,8 @@ public class SalesorderQueryResponseProcessorz {
         for (Object inventoryElement : inventoryElements) {
             if (inventoryElement instanceof SalesOrderLineRet) {
                 PplaOrderItem item = processSalesOrderLineRet((SalesOrderLineRet) inventoryElement);
-                item.setSalesOrder(so);
                 if (null != item) {
+                    item.setSalesOrder(so);
                     orderItems.add(item);
                 }
             }
@@ -95,11 +104,35 @@ public class SalesorderQueryResponseProcessorz {
     }
 
     private PplaOrderItem processSalesOrderLineRet(SalesOrderLineRet inventoryElement) {
+        if (null == inventoryElement.getItemRef() || null == inventoryElement.getItemRef().getListID()) {
+            LOG.warn("Sales order item has no product attached or product has no line id. Skipping.");
+            return null;
+        }
         String productListId = inventoryElement.getItemRef().getListID();
         LOG.debug("Trying to find product by list id. id={}", productListId);
         PplaProduct product = productService.findByInventoryItemListId(productListId);
         if (null == product) {
-            LOG.warn("Product not found!");
+            LOG.warn("Product not found! Trying to create product from inventory.");
+
+            InventoryItem ii = inventoryService.findByListId(productListId);
+            if (null != ii) {
+                inventoryService.createInstanceAs(ii, PplaInventoryType.PRODUCT);
+                product = productService.findByInventoryItemListId(productListId);
+                if (null == product) {
+                    LOG.error("Failed to create product from inventory item! Skipping product.");
+                    return null;
+                }
+            } else {
+                InventoryItem newInventoryItem = new InventoryItem();
+                newInventoryItem.setListId(productListId);
+                newInventoryItem.setDescription(inventoryElement.getDesc());
+                newInventoryItem.setName(inventoryElement.getItemRef().getFullName());
+                newInventoryItem.setType(PplaInventoryType.PRODUCT);
+                newInventoryItem.setTimeModified(DateTime.now());
+                inventoryService.save(newInventoryItem);
+                inventoryService.createInstanceAs(newInventoryItem, PplaInventoryType.PRODUCT);
+                product = productService.findByInventoryItemListId(productListId);
+            }
         } else {
             LOG.debug("Found product. product={}", product.getName());
         }
